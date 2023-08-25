495df748a5c914d5d30669567b56b99b534e08bc
stefanovidmar2005 <stefanovidmar@gmail.com>
2023-08-25 12:41:15 +0300

    RegEx implemented there is now validation on the email input field and errors are being trown where there is an invalid email.

--- main.js
+++ main.js
@@ -8,24 +8,34 @@ const responseSection = document.querySelector(".response");
 const singUpBtn = document.querySelector(".btn");
 const userEmailInput = document.querySelector(".form__group-input");
 const userEmailSpanText = document.querySelector(".user__email");
+const errorMessage = document.querySelector(".error-message");
 
 // Helper function that checkes whether email is valid or not!
-const isEmailValid = (email) => {};
+const isEmailValid = (email) => {
+  let regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return regex.test(String(email).toLowerCase());
+};
 
 const nextPage = (userEmail) => {
-  console.log(userEmail);
-  // guard clause to avoid email is empty
-  if (!userEmail) {
-    userEmailInput.classList.add("error-outline");
-    return;
+  if (isEmailValid(userEmail)) {
+    userEmailInput.value = "";
+    signUpSection.classList.toggle("hidden");
+    responseSection.classList.toggle("hidden");
+    userEmailSpanText.textContent = userEmail;
+  } else {
+    userEmailInput.classList.add("error-input");
+    errorMessage.classList.remove("hidden");
   }
-  userEmailInput.value = "";
-  signUpSection.classList.toggle("hidden");
-  responseSection.classList.toggle("hidden");
-  userEmailSpanText.textContent = userEmail;
 };
 
 singUpBtn.addEventListener("click", (e) => {
   e.preventDefault();
   nextPage(userEmailInput.value);
+});
+userEmailInput.addEventListener("focus", () => {
+  console.log("focus");
+  if (userEmailInput.classList.contains("error-input"))
+    userEmailInput.classList.remove("error-input");
+  if (!errorMessage.classList.contains("hidden"))
+    errorMessage.classList.add("hidden");
 });
